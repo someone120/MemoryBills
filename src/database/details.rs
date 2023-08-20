43ba5b7f2c550fa969aff712fad192b304f31b01
someone120 <mouren1101@outlook.com>
2023-08-21 00:06:59 +0800

    feat: add transaction detail

--- src/database/details.rs
+++ src/database/details.rs
@@ -1,22 +1,33 @@
-use rusqlite::{Connection,Result};
+use rusqlite::{params, Connection, Result};
 
 #[derive(Debug)]
-pub struct Details{
-    pub id:i32,
-    pub trans_id:i32,
-    pub account_id:i32,
-    pub currency:String,
-    pub balance:f32,
+pub struct Details {
+    pub id: i32,
+    pub trans_id: i32,
+    pub account_id: i32,
+    pub currency: String,
+    pub balance: f32,
 }
 
-pub fn add_details(conn: &Connection,trans_id:i32,account_id:i32,currency:String,balance:f32){
+pub fn add_details(
+    conn: &Connection,
+    trans_id: i32,
+    account_id: i32,
+    currency: String,
+    balance: f32,
+)->Result<()> {
+    conn.execute(
+        "INSERT INTO DETAIL (trans_id,account_id,currency,balance) values (?1,?2,?3,?4)",
+        params![trans_id, account_id, currency, balance],
+    )?;
+    Ok(())
+}
+
+pub fn get_details_by_trans(conn: &Connection, trans_id: i32) -> Result<Vec<Details>> {
     todo!()
 }
 
-pub fn get_details_by_trans(conn: &Connection,trans_id:i32) -> Result<Vec<Details>>{
+pub fn get_details_by_account(conn: &Connection, account_id: i32) -> Result<Vec<Details>> {
     todo!()
 }
 
-pub fn get_details_by_account(conn: &Connection,account_id:i32) -> Result<Vec<Details>>{
-    todo!()
-}
