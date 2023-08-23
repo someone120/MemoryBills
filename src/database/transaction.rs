5861aee94505ccc6134cbfceb0eabd6289c8196f
someone120 <mouren1101@outlook.com>
2023-08-23 23:49:14 +0800

    feat: add transaction functions

--- src/database/transaction.rs
+++ src/database/transaction.rs
@@ -1,6 +1,27 @@
+use std::time::TryFromFloatSecsError;
+
+use chrono::{Date, NaiveDate};
+use rusqlite::Connection;
+
 #[derive(Debug)]
-pub struct Transaction{
-    pub id:i32,
-    pub extra:String
+pub struct Transaction {
+    pub id: String,
+    pub date: String,
+    pub extra: String,
 }
 
+pub fn add_transaction(conn: &Connection, date: NaiveDate, extra: &str) {
+    todo!()
+}
+
+pub fn del_transaction(conn: &Connection, id: &str) {
+    todo!()
+}
+
+pub fn get_transactions(conn: &Connection, id: &str) {
+    todo!()
+}
+
+pub fn change_extra(conn: &Connection, id: &str, extra: &str) {
+    todo!()
+}
