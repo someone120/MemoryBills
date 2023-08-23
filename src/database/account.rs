61e8061b47321593f096da47525b94ce213ce2e7
someone120 <mouren1101@outlook.com>
2023-08-23 23:36:29 +0800

    style: format files

--- src/database/account.rs
+++ src/database/account.rs
@@ -1,31 +1,30 @@
-use rusqlite::{Connection,Result, params};
+use rusqlite::{params, Connection, Result};
 use uuid::Uuid;
 
 #[derive(Debug)]
-pub struct Account{
-    pub id:String,
-    pub name:String,
-    pub balance:f32,
-    pub currency:String,
+pub struct Account {
+    pub id: String,
+    pub name: String,
+    pub balance: f32,
+    pub currency: String,
 }
 /// add a new account
-pub fn add_account(conn:&Connection,name:&str,currency:&str)->Result<()>{
-    let id=Uuid::new_v4().to_string();
-    add_account_with_id(conn, name, currency,id.as_str())
+pub fn add_account(conn: &Connection, name: &str, currency: &str) -> Result<()> {
+    let id = Uuid::new_v4().to_string();
+    add_account_with_id(conn, name, currency, id.as_str())
 }
 
-fn add_account_with_id(conn:&Connection,name:&str,currency:&str,id:&str)->Result<()>{
-
+fn add_account_with_id(conn: &Connection, name: &str, currency: &str, id: &str) -> Result<()> {
     conn.execute(
         "INSERT INTO ACCOUNT (id,name,currency,balance) VALUES (?1,?2,?3,?4)",
-        (id,name,currency,0.0),
+        (id, name, currency, 0.0),
     )?;
     Ok(())
 }
 
 /// Get info of account
-pub fn read_account(conn:&Connection,id:&str)->Result<Vec<Account>>{
-    let mut stmt=conn.prepare("SELECT * FROM ACCOUNT WHERE id = ?")?;
+pub fn read_account(conn: &Connection, id: &str) -> Result<Vec<Account>> {
+    let mut stmt = conn.prepare("SELECT * FROM ACCOUNT WHERE id = ?")?;
     let iter = stmt.query_map(params![id], |row| {
         Ok(Account {
             id: row.get(0)?,
@@ -35,21 +34,21 @@ pub fn read_account(conn:&Connection,id:&str)->Result<Vec<Account>>{
         })
     })?;
     let mut result = Vec::new();
-    for i in iter{
+    for i in iter {
         result.push(i?)
     }
     Ok(result)
 }
 
 /// Remove account
-pub fn del_account(conn:&Connection,id:&str)-> Result<()>{
+pub fn del_account(conn: &Connection, id: &str) -> Result<()> {
     conn.execute("DELETE FROM ACCOUNT WHERE id=?1", params![id])?;
     Ok(())
 }
 
 /// Get all account
-pub fn get_accounts(conn:&Connection)->Result<Vec<Account>>{
-    let mut stmt=conn.prepare("SELECT * FROM ACCOUNT")?;
+pub fn get_accounts(conn: &Connection) -> Result<Vec<Account>> {
+    let mut stmt = conn.prepare("SELECT * FROM ACCOUNT")?;
     let iter = stmt.query_map([], |row| {
         Ok(Account {
             id: row.get(0)?,
@@ -59,7 +58,7 @@ pub fn get_accounts(conn:&Connection)->Result<Vec<Account>>{
         })
     })?;
     let mut result = Vec::new();
-    for i in iter{
+    for i in iter {
         result.push(i?)
     }
     Ok(result)
@@ -71,11 +70,11 @@ mod tests {
 
     use super::*;
     #[test]
-    fn test_add_account() -> Result<()>{
+    fn test_add_account() -> Result<()> {
         let conn = Connection::open_in_memory()?;
         init(&conn)?;
         add_account(&conn, "someone", "CNY")?;
-        
+
         let mut stmt = conn.prepare("SELECT id,name,currency,balance FROM ACCOUNT")?;
         let mut iter = stmt.query_map([], |row| {
             Ok(Account {
@@ -91,38 +90,37 @@ mod tests {
         panic!()
     }
     #[test]
-    fn test_read_account() -> Result<()>{
+    fn test_read_account() -> Result<()> {
         let conn = Connection::open_in_memory()?;
         init(&conn)?;
         add_account(&conn, "a", "b")?;
-        let accounts=get_accounts(&conn)?;
-        let a=read_account(&conn,&accounts[0].id)?;
-        println!("{:?}",a);
+        let accounts = get_accounts(&conn)?;
+        let a = read_account(&conn, &accounts[0].id)?;
+        println!("{:?}", a);
 
         Ok(())
     }
     #[test]
-    fn test_get_last_id() -> Result<()>{
+    fn test_get_last_id() -> Result<()> {
         let conn = Connection::open_in_memory()?;
         init(&conn)?;
         add_account(&conn, "a", "b")?;
-        
-        let accounts=get_accounts(&conn)?;
 
-        assert_eq!(1,accounts.len());
+        let accounts = get_accounts(&conn)?;
+
+        assert_eq!(1, accounts.len());
         Ok(())
     }
     #[test]
-    fn test_delete()->Result<()>{
-
+    fn test_delete() -> Result<()> {
         let conn = Connection::open_in_memory()?;
         init(&conn)?;
         add_account(&conn, "a", "b")?;
         add_account(&conn, "a", "b")?;
-        let id=get_accounts(&conn)?;
+        let id = get_accounts(&conn)?;
         del_account(&conn, &id[0].id)?;
-        
-        assert_eq!(1,get_accounts(&conn)?.len());
+
+        assert_eq!(1, get_accounts(&conn)?.len());
         Ok(())
     }
 }
--- src/database/details.rs
+++ src/database/details.rs
@@ -25,41 +25,43 @@ pub fn add_details(
 }
 
 pub fn get_details_by_trans(conn: &Connection, trans_id: i32) -> Result<Vec<Details>> {
-    let mut stmt=conn.prepare("SELECT (id,trans_id,account_id,currency,balance) FROM DETAIL WHERE trans_id=?1")?;
-    let iter=stmt.query_map(params![trans_id], |row|{
-        Ok(Details{
-            id:row.get(0)?,
-            trans_id:row.get(1)?,
-            account_id:row.get(2)?,
-            currency:row.get(3)?,
-            balance:row.get(4)?,
+    let mut stmt = conn.prepare(
+        "SELECT (id,trans_id,account_id,currency,balance) FROM DETAIL WHERE trans_id=?1",
+    )?;
+    let iter = stmt.query_map(params![trans_id], |row| {
+        Ok(Details {
+            id: row.get(0)?,
+            trans_id: row.get(1)?,
+            account_id: row.get(2)?,
+            currency: row.get(3)?,
+            balance: row.get(4)?,
         })
     })?;
 
     let mut result = Vec::new();
-    for i in iter{
+    for i in iter {
         result.push(i?);
     }
 
     Ok(result)
-
 }
 
 pub fn get_details_by_account(conn: &Connection, account_id: i32) -> Result<Vec<Details>> {
-        
-    let mut stmt=conn.prepare("SELECT (id,trans_id,account_id,currency,balance) FROM DETAIL WHERE account_id=?1")?;
-    let iter=stmt.query_map(params![account_id], |row|{
-        Ok(Details{
-            id:row.get(0)?,
-            trans_id:row.get(1)?,
-            account_id:row.get(2)?,
-            currency:row.get(3)?,
-            balance:row.get(4)?,
+    let mut stmt = conn.prepare(
+        "SELECT (id,trans_id,account_id,currency,balance) FROM DETAIL WHERE account_id=?1",
+    )?;
+    let iter = stmt.query_map(params![account_id], |row| {
+        Ok(Details {
+            id: row.get(0)?,
+            trans_id: row.get(1)?,
+            account_id: row.get(2)?,
+            currency: row.get(3)?,
+            balance: row.get(4)?,
         })
     })?;
 
     let mut result = Vec::new();
-    for i in iter{
+    for i in iter {
         result.push(i?);
     }
 
@@ -67,6 +69,4 @@ pub fn get_details_by_account(conn: &Connection, account_id: i32) -> Result<Vec<
 }
 
 #[cfg(test)]
-mod tests{
-    
-}
+mod tests {}
