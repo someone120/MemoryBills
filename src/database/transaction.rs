f4b49393bb5778efb53794f09cb98d6bf13d95c4
someone120 <mouren1101@outlook.com>
2023-08-24 13:19:55 +0800

    feat: implement transaction functions

--- src/database/transaction.rs
+++ src/database/transaction.rs
@@ -1,7 +1,8 @@
 use std::time::TryFromFloatSecsError;
 
 use chrono::{Date, NaiveDate};
-use rusqlite::Connection;
+use rusqlite::{params, Connection, Result};
+use uuid::Uuid;
 
 #[derive(Debug)]
 pub struct Transaction {
@@ -10,31 +11,51 @@ pub struct Transaction {
     pub extra: String,
 }
 
-pub fn add_transaction(conn: &Connection, date: NaiveDate, extra: &str) {
-    todo!()
+pub fn add_transaction(conn: &Connection, date: NaiveDate, extra: &str) -> Result<()> {
+    let id = Uuid::new_v4().to_string();
+    conn.execute(
+        "INSERT INTO TRANS (id,time,extra) VALUES (?1,?2,?3)",
+        (id.as_str(), date.format("%Y-%m-%d %H:%").to_string(), extra),
+    )?;
+    Ok(())
 }
 
-pub fn del_transaction(conn: &Connection, id: &str) {
-    todo!()
+pub fn del_transaction(conn: &Connection, id: &str) -> Result<()> {
+    conn.execute("DELETE FROM TRANS WHERE id=?1", params![id])?;
+    Ok(())
 }
 
-pub fn get_transactions(conn: &Connection, id: &str) {
-    todo!()
+pub fn get_transactions(conn: &Connection, id: &str) -> Result<Vec<Transaction>> {
+    let mut stmt = conn.prepare("SELECT * FROM TRANS WHERE id=?1")?;
+    let iter = stmt.query_map(params![id], |row| {
+        Ok(Transaction {
+            id: row.get(0)?,
+            date: row.get(1)?,
+            extra: row.get(2)?,
+        })
+    })?;
+    let mut result = Vec::new();
+    for i in iter {
+        result.push(i?)
+    }
+    Ok(result)
 }
 
-pub fn change_extra(conn: &Connection, id: &str, extra: &str) {
-    todo!()
+pub fn change_extra(conn: &Connection, id: &str, extra: &str) -> Result<()> {
+    conn.execute(
+        "UPDATE TRANS SET extra = ?1 WHERE id = ?2;",
+        params![extra, id],
+    )?;
+    Ok(())
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     #[test]
-    fn test_add_transaction(){
+    fn test_add_transaction() {
         todo!()
     }
 
     #[test]
-    fn test_del_transaction(){
-
-    }
+    fn test_del_transaction() {}
 }
