1f3ca088873ea3b254e3e6541574e3dc279100b4
someone120 <mouren1101@outlook.com>
2023-08-25 19:59:20 +0800

    feat: add not implemented function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,15 @@
+use rusqlite::Connection;
+use error::Error;
+
 mod database;
 mod error;
 
+pub fn recalcute(conn:&Connection)->Result<(),Error>{
+    database::account::get_accounts(conn)?;
+    todo!()
+}
+
+pub fn verification(conn:&Connection){
+    todo!()
+}
 
